636417c2294e3aaed489a5424b1cc144bbed3336
tycoon87 <tyson.woodruff@outlook.com>
2017-12-04 11:05:06 -0800

    working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  imageArray = [];
+
+  fillImageArray() {
+    for (let y = 0; y < 10; y++) {
+      const randNum = (Math.floor(Math.random() * 6) ) + 1;
+      if (randNum === 1) {
+        this.imageArray.push('DarkSeaGreen ');
+      } else if (randNum === 2) {
+        this.imageArray.push('MediumBlue ');
+      } else if (randNum === 3) {
+        this.imageArray.push('MediumAquaMarine ');
+      } else if (randNum === 4) {
+        this.imageArray.push('DarkBlue ');
+      } else if (randNum === 5) {
+        this.imageArray.push('CadetBlue ');
+      } else if (randNum === 6) {
+        this.imageArray.push('Chartreuse');
+      } else if (randNum === 7) {
+        this.imageArray.push('IndianRed');
+      }
+    }
+  }
+
+  ngOnInit() {
+    this.fillImageArray();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import { MyComponentComponent } from './my-component/my-component.component';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyComponentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
